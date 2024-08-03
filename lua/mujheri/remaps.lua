7b3ccdfa7b3ab2f0d183ab04ad66b7d73849f262
Mudassir Nizamani <mudasiralinizamani@gmail.com>
2024-08-03 13:03:17 +0500

    added spectre keymaps in remaps.lua file

--- lua/mujheri/remaps.lua
+++ lua/mujheri/remaps.lua
@@ -34,3 +34,19 @@ vim.keymap.set("v", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left wind
 vim.keymap.set("v", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("v", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("v", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+-- Spectre
+
+vim.keymap.set('n', '<leader>fs', '<cmd>lua require("spectre").toggle()<CR>', {
+	desc = "Toggle Spectre"
+})
+vim.keymap.set('n', '<leader>fw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+	desc = "Search current word"
+})
+vim.keymap.set('v', '<leader>fw', '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+	desc = "Search current word"
+})
+vim.keymap.set('n', '<leader>fp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+	desc = "Search on current file"
+})
--- lua/mujheri/sets.lua
+++ lua/mujheri/sets.lua
@@ -10,9 +10,10 @@ vim.cmd("let g:netrw_liststyle = 3")
 
 -- Make line numbers default
 vim.opt.number = true
+
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = true
+-- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = "a"
@@ -76,3 +77,6 @@ opt.expandtab = true
 opt.autoindent = true
 
 opt.termguicolors = true
+
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
